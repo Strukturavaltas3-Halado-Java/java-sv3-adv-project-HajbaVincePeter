4b5adb7d51d27a2e9a29a086459e3d6f63d3e974
hajbavince <hajbavince@gmail.com>
2023-04-26 11:18:57 +0200

    crossOrigin for each endpoint

--- restpost/src/main/java/com/example/restpost/controller/ShipmentController.java
+++ restpost/src/main/java/com/example/restpost/controller/ShipmentController.java
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/shipments")
